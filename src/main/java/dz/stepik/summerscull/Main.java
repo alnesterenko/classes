ccf7bbb726ac67a43aca0fcf28ced9c38b981740
alnesterenko <alzpupkin@gmail.com>
2024-05-13 10:39:38 +0300

    Методы класса Scanner и другое.

--- src/main/java/dz/stepik/summerscull/Main.java
+++ src/main/java/dz/stepik/summerscull/Main.java
@@ -1,339 +1,126 @@
 package dz.stepik.summerscull;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
-        int matematicBalsOfTown = 0;
-        int russianLanguageBalsOfTown = 0;
-        int informaticBalsOfTown = 0;
-        int allBalsOfTown = 0;
-
-        int maxBalsInMath = 0;
-        int maxBalsInRusLang = 0;
-        int maxBalsInInform = 0;
-
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-
-        ArrayList <Student> listOfStudents = new ArrayList<>();
-        ArrayList <Scool> listOfScools = new ArrayList<>();
-        ArrayList<Student> listBestsInMath = new ArrayList<>();
-        ArrayList<Student> listBestsInRusLang = new ArrayList<>();
-        ArrayList<Student> listBestsInInform = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            String [] nextLine = scanner.nextLine().trim().split(" ");
-            listOfStudents.add(new Student(nextLine[0], nextLine[1], Integer.parseInt(nextLine[2]), Integer.parseInt(nextLine[3]), Integer.parseInt(nextLine[4]), Integer.parseInt(nextLine[5])));
-           if(listOfScools.size() == 0){
-               listOfScools.add(new Scool(Integer.parseInt(nextLine[2]), 1, Integer.parseInt(nextLine[3]), Integer.parseInt(nextLine[4]), Integer.parseInt(nextLine[5])));
-           }else {
-               boolean haveInList = false;
-               for (int j = 0; j < listOfScools.size(); j++) {
-                   if (listOfScools.get(j).getNumber() == Integer.parseInt(nextLine[2])){
-                       listOfScools.get(j).addNewStudent(Integer.parseInt(nextLine[3]), Integer.parseInt(nextLine[4]), Integer.parseInt(nextLine[5]));
-                       haveInList = true;
-                       break;
-                   }
-               }
-               if (haveInList == false){
-                   listOfScools.add(new Scool(Integer.parseInt(nextLine[2]), 1, Integer.parseInt(nextLine[3]), Integer.parseInt(nextLine[4]), Integer.parseInt(nextLine[5])));
-               }
-           }
-           //Подсчёт среднегородского бала
-
-            matematicBalsOfTown += Integer.parseInt(nextLine[3]);
-           russianLanguageBalsOfTown += Integer.parseInt(nextLine[4]);
-           informaticBalsOfTown += Integer.parseInt(nextLine[5]);
-           allBalsOfTown += Integer.parseInt(nextLine[3]) + Integer.parseInt(nextLine[4]) + Integer.parseInt(nextLine[5]);
-
-            //Конец подсчёта среднегородского бала
+        ParametrList<Integer> arrInt = new ParametrList<>();
+//        arrInt.add(123);
+//        arrInt.add(456);
+//        arrInt.add(901);
+//        System.out.println(arrInt.remove(1));
+//        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            arrInt.add(i);
         }
+        arrInt.add(101);
+        arrInt.add(102);
 
-        // Подсчёт студентов с лучшими балами
-        for (int i = 0; i < listOfStudents.size(); i++) {
-            if (i == 0){
-                maxBalsInMath = listOfStudents.get(i).getMatematicBal();
-                maxBalsInRusLang = listOfStudents.get(i).getRussianLanguageBal();
-                maxBalsInInform = listOfStudents.get(i).getInformaticBal();
-            }
-            if (listOfStudents.get(i).getMatematicBal() > maxBalsInMath){
-                maxBalsInMath = listOfStudents.get(i).getMatematicBal();
-            }
-            if (listOfStudents.get(i).getRussianLanguageBal() > maxBalsInRusLang){
-                maxBalsInRusLang = listOfStudents.get(i).getRussianLanguageBal();
-            }
-            if (listOfStudents.get(i).getInformaticBal() > maxBalsInInform){
-                maxBalsInInform = listOfStudents.get(i).getInformaticBal();
-            }
-        }
-        // Забиваем студентов с лучшими балами в списки
-        for (int i = 0; i < listOfStudents.size(); i++) {
+        System.out.println(arrInt.remove(2));
+        System.out.println(arrInt.toString());
 
-            if (listOfStudents.get(i).getMatematicBal() == maxBalsInMath){
-                listBestsInMath.add(listOfStudents.get(i));
-            }
-            if (listOfStudents.get(i).getRussianLanguageBal() == maxBalsInRusLang){
-                listBestsInRusLang.add(listOfStudents.get(i));
-            }
-            if (listOfStudents.get(i).getInformaticBal() == maxBalsInInform){
-                listBestsInInform.add(listOfStudents.get(i));
-            }
-        }
-        // Сортируем списки лучших школоблогеров )))))))
-        listBestsInMath.sort(Comparator.comparing(Student::getLastname)
-                        .thenComparing(Student::getName));
-
-        listBestsInRusLang.sort(Comparator.comparing(Student::getLastname)
-                .thenComparing(Student::getName));
-
-        listBestsInInform.sort(Comparator.comparing(Student::getLastname)
-                .thenComparing(Student::getName));
-
-        // ***********************************
-
-
-        // Вывод
-
-        System.out.println("Отчет по городу: математика - " + String.format(Locale.ENGLISH,"%.1f", ((double)matematicBalsOfTown) / n) + ", русский язык - " + String.format(Locale.ENGLISH,"%.1f", ((double)russianLanguageBalsOfTown) / n) + ", инфрматика - " + String.format(Locale.ENGLISH,"%.1f", ((double)informaticBalsOfTown) / n) + ", общий средний балл - " + String.format(Locale.ENGLISH,"%.1f", ((double)allBalsOfTown) / (n * 3)));
-        System.out.println("Отчет по школам:");
-
-        // Сортировка школ
-        listOfScools.sort(Comparator.comparing(Scool::getAllSrBalsInThisScool)
-                .thenComparing(Scool::getMatematicSrBals).reversed()
-                .thenComparing(Scool::getRussianLanguageSrBals).reversed()
-                .thenComparing(Scool::getInformaticSrBals).reversed()
-                .thenComparing(Scool::getNumber).reversed());
-        // Конец сортировки школ
-
-        for (Scool oneScool :
-                listOfScools) {
-            System.out.println(oneScool.toString());
-        }
-
-        // Вывод лучших школоблогеров
-
-        for (Student oneStudent :
-                listBestsInMath) {
-            System.out.println("Лучший результат по математике - "
-                    + oneStudent.getLastname() + " "
-                    + oneStudent.getName() + " - "
-                    + oneStudent.getMatematicBal());
-        }
-        for (Student oneStudent :
-                listBestsInRusLang) {
-            System.out.println("Лучший результат по русскому языку - "
-                    + oneStudent.getLastname() + " "
-                    + oneStudent.getName() + " - "
-                    + oneStudent.getRussianLanguageBal());
-        }
-        for (Student oneStudent :
-                listBestsInInform) {
-            System.out.println("Лучший результат по информатике - "
-                    + oneStudent.getLastname() + " "
-                    + oneStudent.getName() + " - "
-                    + oneStudent.getInformaticBal());
-        }
-
-        // ВСЁ!!!! Вывод ушлёпков закончен
 
     }
-
 }
 
-class Student{
-    private String lastname, name;
-    private int scoolNumber, matematicBal, russianLanguageBal, informaticBal;
+class ParametrList<E>{
+    private Object [] data;
+    private int size;
+    private int razm = 100;
 
-    public Student(String lastname,String name, int scoolNumber, int matematicBal, int russianLanguageBal, int informaticBal) {
-        this.lastname = lastname;
-        this.name = name;
-        this.scoolNumber = scoolNumber;
-        this.matematicBal = matematicBal;
-        this.russianLanguageBal = russianLanguageBal;
-        this.informaticBal = informaticBal;
+    public ParametrList(){
+        size = 0;
+        data = new Object[razm];
     }
 
-    public String getLastname() {
-        return lastname;
+    public int size(){
+        return size;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public void add(E element){
+        if (size >= razm){
+            Object [] tempData = new Object[size() + 1];
+            for (int i = 0; i < size(); i++) {
+                tempData[i] = data[i];
+            }
+            tempData[size()] = element;
+            size++;
+            data = tempData;
+        }else {
+            data[size] = element;
+            size++;
+//            razm = size();
+        }
     }
 
-    public String getName() {
-        return name;
+    public E get(int index){
+        return (E) data[index];
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void set(int index, E value){
+        data[index] = value;
     }
 
-    public int getScoolNumber() {
-        return scoolNumber;
+    public E remove(int index){
+        E temp = (E) data[index];
+//        data[index] = null;
+        Object [] tempData = new Object[razm];
+        int i, j;
+        for (i = 0, j = 0; i < size(); i++, j++) {
+            if (i == index){
+                j--;
+                continue;
+            }else {
+                tempData[j] = data[i];
+            }
+        }
+        size--;
+        data = tempData;
+        return temp;
     }
 
-    public void setScoolNumber(int scoolNumber) {
-        this.scoolNumber = scoolNumber;
+    public void insert(int index, E value){
+        E temp = value;
+        int newRazm;
+        if (size > razm){
+            newRazm = size + 1;
+        }else {
+            newRazm = razm;
+        }
+
+        Object [] tempData = new Object[newRazm];
+        int i, j;
+        for (i = 0, j = 0; i < size(); i++, j++) {
+            if (j == index){
+                tempData[index] = temp;
+                i--;
+
+            }else {
+                tempData[j] = data[i];
+            }
+        }
+        size++;
+        data = tempData;
     }
 
-    public int getMatematicBal() {
-        return matematicBal;
-    }
-
-    public void setMatematicBal(int matematicBal) {
-        this.matematicBal = matematicBal;
-    }
-
-    public int getRussianLanguageBal() {
-        return russianLanguageBal;
-    }
-
-    public void setRussianLanguageBal(int russianLanguageBal) {
-        this.russianLanguageBal = russianLanguageBal;
-    }
-
-    public int getInformaticBal() {
-        return informaticBal;
-    }
-
-    public void setInformaticBal(int informaticBal) {
-        this.informaticBal = informaticBal;
+    public int find(E value){
+        for (int i = 0; i < size; i++) {
+            if (value == data[i]){
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
-    public String toString() {
-        return lastname + " " + name + " " + scoolNumber + " " + matematicBal + " " + russianLanguageBal + " " + informaticBal;
-    }
-}
-class Scool{
-    private int number;
-    int countOfStuding;
-    private int matematicCountBals, russianLanguageCountBals, informaticCountBals, allCountBalsInThisScool;
-    private double matematicSrBals, russianLanguageSrBals, informaticSrBals, allSrBalsInThisScool;
-
-    public Scool(int number, int countOfStuding,  int matematicCountBals, int russianLanguageCountBals, int informaticCountBals) {
-        this.number = number;
-        this.countOfStuding = countOfStuding;
-        this.matematicCountBals = matematicCountBals;
-        this.russianLanguageCountBals = russianLanguageCountBals;
-        this.informaticCountBals = informaticCountBals;
-
-        setAllCountBalsInThisScool(this.getMatematicCountBals(), this.getRussianLanguageCountBals(), this.getInformaticCountBals());
-        // Подсчёт средних балов
-        setMatematicSrBals(this.getMatematicCountBals());
-        setRussianLanguageSrBals(this.getRussianLanguageCountBals());
-        setInformaticSrBals(this.getInformaticCountBals());
-        setAllSrBalsInThisScool(this.getAllCountBalsInThisScool());
-        //********************************************************
-    }
-    public void addNewStudent(int matematicCountBals, int russianLanguageCountBals, int informaticCountBals){
-        this.countOfStuding++;
-        setMatematicCountBals(matematicCountBals);
-        setRussianLanguageCountBals(russianLanguageCountBals);
-        setInformaticCountBals(informaticCountBals);
-
-        setAllCountBalsInThisScool(this.getMatematicCountBals(), this.getRussianLanguageCountBals(), this.getInformaticCountBals());
-        // Подсчёт средних балов
-        setMatematicSrBals(this.getMatematicCountBals());
-        setRussianLanguageSrBals(this.getRussianLanguageCountBals());
-        setInformaticSrBals(this.getInformaticCountBals());
-        setAllSrBalsInThisScool(this.getAllCountBalsInThisScool());
-        //********************************************************
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public int getCountOfStuding() {
-        return countOfStuding;
-    }
-
-    public void setCountOfStuding(int countOfStuding) {
-        this.countOfStuding++;
-    }
-
-    public int getMatematicCountBals() {
-        return matematicCountBals;
-    }
-
-    public void setMatematicCountBals(int matematicCountBals) {
-        this.matematicCountBals += matematicCountBals;
-    }
-
-    public int getRussianLanguageCountBals() {
-        return russianLanguageCountBals;
-    }
-
-    public void setRussianLanguageCountBals(int russianLanguageCountBals) {
-        this.russianLanguageCountBals += russianLanguageCountBals;
-    }
-
-    public int getInformaticCountBals() {
-        return informaticCountBals;
-    }
-
-    public void setInformaticCountBals(int informaticCountBals) {
-        this.informaticCountBals += informaticCountBals;
-    }
-
-    public int getAllCountBalsInThisScool() { return allCountBalsInThisScool; }
-
-    public void setAllCountBalsInThisScool(int matematicCountBals, int russianLanguageCountBals, int informaticCountBals) {
-        this.allCountBalsInThisScool = matematicCountBals + russianLanguageCountBals + informaticCountBals;
-    }
-    //********************************************************************************************
-
-
-    public double getMatematicSrBals() {
-        return matematicSrBals;
-    }
-
-    public void setMatematicSrBals(int matematicCountBals) {
-        this.matematicSrBals = ((double)matematicCountBals / countOfStuding);
-    }
-
-    public double getRussianLanguageSrBals() {
-        return russianLanguageSrBals;
-    }
-
-    public void setRussianLanguageSrBals(int russianLanguageCountBals) {
-        this.russianLanguageSrBals =  ((double)russianLanguageCountBals / countOfStuding);
-    }
-
-    public double getInformaticSrBals() {
-        return informaticSrBals;
-    }
-
-    public void setInformaticSrBals(int informaticCountBals) {
-        this.informaticSrBals = ((double)informaticCountBals / countOfStuding);
-    }
-
-    public double getAllSrBalsInThisScool() {
-        return allSrBalsInThisScool;
-    }
-
-    public void setAllSrBalsInThisScool(int allCountBalsInThisScool) {
-        this.allSrBalsInThisScool = ((double)allCountBalsInThisScool / (getCountOfStuding() * 3));
-    }
-    //***********************************************************************************************************
-
-    @Override
-    public String toString() {
-
-        return "Школа № " + getNumber() +
-        ": математика - " + String.format(Locale.ENGLISH,"%.1f", getMatematicSrBals()) +
-                ", русский язык - " + String.format(Locale.ENGLISH,"%.1f", getRussianLanguageSrBals()) +
-                ", инфрматика - " + String.format(Locale.ENGLISH,"%.1f", getInformaticSrBals()) +
-                ", общий средний балл - " + String.format(Locale.ENGLISH,"%.1f", getAllSrBalsInThisScool());
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            s.append(data[i]).append(" ");
+        }
+        return s.toString();
     }
 }
